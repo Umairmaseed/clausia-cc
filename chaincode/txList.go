b4d13c8da8172f26b29eddf8dbca1d3f69e4fa7b
Umair Khan <110655403+Umairmaseed@users.noreply.github.com>
2024-05-28 14:40:09 +0500

    upload document transaction created

--- chaincode/txList.go
+++ chaincode/txList.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	tx "github.com/hyperledger-labs/cc-tools/transactions"
+	"github.com/hyperledger-labs/goprocess-cc/chaincode/txdefs"
 )
 
 var txList = []tx.Transaction{
 	tx.CreateAsset,
 	tx.UpdateAsset,
 	tx.DeleteAsset,
+	txdefs.UploadDocument,
 }
