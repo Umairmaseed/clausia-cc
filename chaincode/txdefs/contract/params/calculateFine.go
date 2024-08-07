e4e4a92d8e8c7b62ad9fe5f1fd653cb72403a0b9
Umair Khan <110655403+Umairmaseed@users.noreply.github.com>
2024-08-07 18:46:03 +0500

    Calculate fine clause defination added

---
+++ chaincode/txdefs/contract/params/calculateFine.go
@@ -0,0 +1,103 @@
+package params
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/hyperledger-labs/cc-tools/errors"
+	"github.com/hyperledger-labs/goprocess-cc/chaincode/datatypes"
+	"github.com/hyperledger-labs/goprocess-cc/chaincode/txdefs/contract/models"
+)
+
+const (
+	fineName = "calculateFine"
+)
+
+type CalculateFineParameters struct {
+	ImposeFine        bool    `json:"imposeFine"`
+	FineName          string  `json:"fineName"`
+	MaxPercentage     float64 `json:"maxPercentage"`
+	MaxReferenceValue float64 `json:"maxReferenceValue"`
+}
+
+type CalculateFineInput struct {
+	ReferenceValue  float64 `json:"referenceValue"`
+	DailyPercentage float64 `json:"dailyPercentage"`
+	Days            float64 `json:"days"`
+}
+
+type CalculateFine struct{}
+
+func (a *CalculateFine) Type() datatypes.ActionType {
+	return datatypes.GetDeduction
+}
+
+func (a *CalculateFine) GetParameters() interface{} {
+	return CalculateFineParameters{}
+}
+
+func (a *CalculateFine) GetInputs() interface{} {
+	return CalculateFineInput{}
+}
+
+func (a *CalculateFine) Execute(input interface{}) (*models.Result, bool, errors.ICCError) {
+	inputBytes, err := json.Marshal(input)
+	if err != nil {
+		return nil, false, errors.WrapError(err, "failed to marshal input")
+	}
+
+	var fineInput CalculateFineInput
+	err = json.Unmarshal(inputBytes, &fineInput)
+	if err != nil {
+		return nil, false, errors.WrapError(err, "Failed to unmarshal input")
+	}
+
+	var fineParams CalculateFineParameters
+	err = json.Unmarshal(inputBytes, &fineParams)
+	if err != nil {
+		return nil, false, errors.WrapError(err, "Failed to unmarshal parameters")
+	}
+
+	if !fineParams.ImposeFine {
+		return &models.Result{
+			Success:  true,
+			Feedback: "Fine calculation is not imposed.",
+		}, false, nil
+	}
+
+	if fineInput.ReferenceValue <= 0 || fineInput.DailyPercentage <= 0 || fineInput.Days <= 0 {
+		return nil, false, errors.NewCCError("Invalid input values: ReferenceValue, DailyPercentage, and Days should be greater than 0", http.StatusBadRequest)
+	}
+
+	// Calculate the fine
+	fine := fineInput.ReferenceValue * fineInput.DailyPercentage / 100 * fineInput.Days
+
+	// Apply upper limit if necessary
+	var shouldConsiderUpperLimit bool
+	if fineParams.MaxPercentage > 0 && fineParams.MaxReferenceValue > 0 {
+		shouldConsiderUpperLimit = true
+	}
+
+	if shouldConsiderUpperLimit {
+		limit := fineParams.MaxPercentage / 100 * fineParams.MaxReferenceValue
+		if fine > limit {
+			fine = limit
+		}
+	}
+
+	// Handle empty fine name
+	if fineParams.FineName == "" {
+		fineParams.FineName = fineName
+	}
+
+	// Prepare result
+	result := &models.Result{
+		Success:  true,
+		Feedback: "Fine calculated successfully.",
+		Data: map[string]interface{}{
+			fineParams.FineName: fine,
+		},
+	}
+
+	return result, true, nil
+}
