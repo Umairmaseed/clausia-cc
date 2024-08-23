112aa7ff2c42c3afb3d9a0ebe1f8a8c2b7e1310b
Umair Khan <110655403+Umairmaseed@users.noreply.github.com>
2024-08-23 16:24:49 +0500

    payment asset created

---
+++ chaincode/assettypes/contractassettypes/payment.go
@@ -0,0 +1,50 @@
+package contractassettypes
+
+import "github.com/hyperledger-labs/cc-tools/assets"
+
+var Payment = assets.AssetType{
+	Tag:         "payment",
+	Label:       "Payment",
+	Description: "Payment",
+
+	Props: []assets.AssetProp{
+		{
+			Required: true,
+			Tag:      "name",
+			Label:    "name",
+			DataType: "string",
+		},
+		{
+			Required: true,
+			IsKey:    true,
+			Tag:      "receiptHash",
+			Label:    "receiptHash",
+			DataType: "sha256",
+		},
+		{
+			Required: true,
+			Tag:      "receiptUrl",
+			Label:    "receiptUrl",
+			DataType: "string",
+		},
+		{
+			Required: true,
+			Tag:      "payment",
+			Label:    "payment",
+			DataType: "number",
+		},
+		{
+			Required: true,
+			Tag:      "autoExecutableContract",
+			Label:    "autoExecutableContract",
+			DataType: "->autoExecutableContract",
+		},
+		{
+			Required: true,
+			IsKey:    true,
+			Tag:      "clause",
+			Label:    "clause",
+			DataType: "->clause",
+		},
+	},
+}
