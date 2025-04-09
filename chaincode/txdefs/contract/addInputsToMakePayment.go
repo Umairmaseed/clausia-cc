7f500e62761fa46aab0dea36bdde5504abd81f32
Umair Khan <110655403+Umairmaseed@users.noreply.github.com>
2025-04-09 19:29:48 +0500

    refactore payment input to extract date and save to contract

--- chaincode/txdefs/contract/addInputsToMakePayment.go
+++ chaincode/txdefs/contract/addInputsToMakePayment.go
@@ -2,6 +2,7 @@ package contract
 
 import (
 	"encoding/json"
+	"net/http"
 
 	"github.com/hyperledger-labs/cc-tools/assets"
 	"github.com/hyperledger-labs/cc-tools/errors"
@@ -68,6 +69,33 @@ var AddInputsToMakePaymentClause = tx.Transaction{
 			return nil, errors.NewCCError("Invalid clause format", 400)
 		}
 
+		query := map[string]interface{}{
+			"selector": map[string]interface{}{
+				"@assetType": "autoExecutableContract",
+				"clauses": map[string]interface{}{
+					"$elemMatch": clauseKey,
+				},
+			},
+		}
+
+		response, err := assets.Search(stub, query, "", true)
+		if err != nil {
+			return nil, errors.WrapErrorWithStatus(err, "error searching for query", http.StatusInternalServerError)
+		}
+
+		contractKey := assets.Key{}
+		if len(response.Result) != 0 {
+			contractKey = assets.Key{
+				"@assetType": "autoExecutableContract",
+				"@key":       response.Result[0]["@key"],
+			}
+		}
+
+		contractAsset, err := contractKey.Get(stub)
+		if err != nil {
+			return nil, errors.WrapError(err, "Failed to get autoExecutableContract asset from ledger")
+		}
+
 		clauseAsset, err := clauseKey.Get(stub)
 		if err != nil {
 			return nil, errors.WrapError(err, "Failed to get clause asset from ledger")
@@ -76,6 +104,11 @@ var AddInputsToMakePaymentClause = tx.Transaction{
 		actionType, ok := (*clauseAsset)["actionType"].(datatypes.ActionType)
 		if !ok {
 			return nil, errors.NewCCError("Invalid action type format", 400)
+		}
+
+		paymentName, ok := (*clauseAsset)["parameters"].(map[string]interface{})["name"].(string)
+		if !ok {
+			return nil, errors.NewCCError("Invalid payment name format", 400)
 		}
 
 		if actionType == datatypes.Payment {
@@ -114,6 +147,21 @@ var AddInputsToMakePaymentClause = tx.Transaction{
 			})
 			if err != nil {
 				return nil, errors.WrapError(err, "Failed to update clause")
+			}
+
+			contractDates, ok := (*contractAsset)["dates"].(map[string]interface{})
+			if !ok {
+				contractDates = make(map[string]interface{})
+			}
+
+			contractDates[paymentName] = req["date"]
+
+			_, err = contractAsset.Update(stub, map[string]interface{}{
+				"dates": contractDates,
+			})
+
+			if err != nil {
+				return nil, errors.WrapError(err, "Failed to update contract asset")
 			}
 
 			response, nerr := json.Marshal(clauseUpdated)
